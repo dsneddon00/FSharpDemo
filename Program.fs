cf7630785e6b5cdb00686625cde279488174ad39
LangHui He <langhuihe@gmail.com>
2021-12-05 21:52:49 -0700

    add Object Programming

--- Program.fs
+++ Program.fs
@@ -2,7 +2,151 @@ module HelloSquare
 
 let square x = x * x
 
-[<EntryPoint>]
-let main argv =
+// [<EntryPoint>]
+let argv =
     printfn "%d squared is: %d!" 12 (square 12)
     0 // Return an integer exit code
+
+// let line = "-------------------------"
+// printfn "%A" line 
+printfn "-------------------------"
+
+printfn "Object Programming"
+// This class has a primary constructor that takes three arguments
+// and an additional constructor that calls the primary constructor.
+type MyClass(x0, y0, z0) =
+    let mutable x = x0
+    let mutable y = y0
+    let mutable z = z0
+    do
+        printfn "Initialized object that has coordinates (%d, %d, %d)" x y z
+    member this.X with get() = x and set(value) = x <- value
+    member this.Y with get() = y and set(value) = y <- value
+    member this.Z with get() = z and set(value) = z <- value
+
+    // default value for the instance
+    new() = MyClass(0, 0, 0)
+
+// Create by using the new keyword.
+let myObject1 = new MyClass(1, 2, 3)
+// Create without using the new keyword.
+let myObject2 = MyClass(4, 5, 6)
+// Create by using named arguments.
+let myObject3 = MyClass(x0 = 7, y0 = 8, z0 = 9)
+// Create by using the additional constructor.
+let myObject4 = MyClass()
+
+printfn ""
+
+// Generic Type Parameters
+type MyGenericClass<'a> (x: 'a) =
+   do printfn "%A" x
+
+let g1 = MyGenericClass( seq { for i in 1 .. 10 -> (i, i*i) } )
+(*
+
+In golang
+for i := 1; i <= 10; i++ {
+   fmt.Printf("(%d, %d)", i, i*i)
+}
+
+In python
+lst = []
+for i in range(1, 11):
+    lst.append((i,i*i))
+print(lst)
+
+*)
+
+// Sequence that has an increment.
+// seq { 0 .. 10 .. 100 }
+
+printfn ""
+
+// import
+open System.IO
+
+type Folder(pathIn: string) =
+  let path = pathIn
+  let filenameArray : string array = Directory.GetFiles(path)
+  member this.FileArray = Array.map (fun elem -> new File(elem, this)) filenameArray
+
+// like sub-class in python
+and File(filename: string, containingFolder: Folder) =
+   member this.Name = filename
+   member this.ContainingFolder = containingFolder
+
+let folder1 = new Folder(".")
+
+for file in folder1.FileArray do
+   printfn "%s" file.Name
+
+printfn ""
+
+type SomeType(factor0: int) =
+   let factor = factor0
+   member this.SomeMethod(a, b, c) =
+      (a + b + c) * factor
+
+   member this.SomeOtherMethod(a, b, c) =
+      this.SomeMethod(a, b, c) * factor
+
+let test1 = SomeType(2)
+
+printfn "SomeType.SomeMethod : %d" (test1.SomeMethod(3, 3, 3))
+printfn "SomeType.SomeOtherMethod : %d" (test1.SomeOtherMethod(3, 3, 3))
+
+type Ellipse(a0 : float, b0 : float, theta0 : float) =
+    let mutable axis1 = a0
+    let mutable axis2 = b0
+    let mutable rotAngle = theta0
+    abstract member Rotate: float -> unit
+    default this.Rotate(delta : float) = rotAngle <- rotAngle + delta
+type Circle(radius : float) =
+    inherit Ellipse(radius, radius, 0.0)
+     // Circles are invariant to rotation, so do nothing.
+    override this.Rotate(_) = ()
+
+
+printfn ""
+
+type RectangleXY(x1 : float, y1: float, x2: float, y2: float) =
+    // Field definitions.
+    let height = y2 - y1
+    let width = x2 - x1
+    let area = height * width
+    // Private functions.
+    static let maxFloat (x: float) (y: float) =
+      if x >= y then x else y
+    static let minFloat (x: float) (y: float) =
+      if x <= y then x else y
+    // Properties.
+    // Here, "this" is used as the self identifier,
+    // but it can be any identifier.
+    member this.X1 = x1
+    member this.Y1 = y1
+    member this.X2 = x2
+    member this.Y2 = y2
+    // A static method.
+    static member intersection(rect1 : RectangleXY, rect2 : RectangleXY) =
+       let x1 = maxFloat rect1.X1 rect2.X1
+       let y1 = maxFloat rect1.Y1 rect2.Y1
+       let x2 = minFloat rect1.X2 rect2.X2
+       let y2 = minFloat rect1.Y2 rect2.Y2
+       let result : RectangleXY option =
+         if ( x2 > x1 && y2 > y1) then
+           Some (RectangleXY(x1, y1, x2, y2))
+         else
+           None
+       result
+
+// Test code.
+let testIntersection =
+    let r1 = RectangleXY(10.0, 10.0, 20.0, 20.0)
+    let r2 = RectangleXY(15.0, 15.0, 25.0, 25.0)
+    let r3 : RectangleXY option = RectangleXY.intersection(r1, r2)
+    match r3 with
+    | Some(r3) -> printfn "Intersection rectangle: %f %f %f %f" r3.X1 r3.Y1 r3.X2 r3.Y2
+    | None -> printfn "No intersection found."
+
+testIntersection
